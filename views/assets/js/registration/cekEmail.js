5113677aa576c3e5551e78ead6f6722fd90ae17c
Wildan Aizzaddin <aizzaddin@gmail.com>
2021-03-05 14:45:33 +0700

    add new base

--- views/assets/js/registration/cekEmail.js
+++ views/assets/js/registration/cekEmail.js
@@ -1,26 +1,24 @@
 // CEK EMAIL
 
-$(function email() {
-    $("#btnSubmited").click(function () {
-        let mail = $("#txtEmail").val();
-        let atps=mail.indexOf("@");
-        let dots=mail.lastIndexOf(".");
-        if (mail=='') {
-            $("#valMail").html("* Email masih kosong");
-            $("#valMail").css("display","block")
-            $("#valMail").css("color","red")
-        }
-        else if (atps<1 || dots<atps+2 ) {
-            $("#valMail").html("* Email tidak valid");
-            $("#valMail").css("display","block")
-            $("#valMail").css("color","red")
-            return false;
-        }
-        else {
-            $("#valMail").html("* Email valid");
-            $("#valMail").css("display","block")
-            $("#valMail").css("color","green")
-            return true;
-        }
-    });
+$("#txtEmail").on('keyup',function () {
+    let mail = $("#txtEmail").val();
+    let atps=mail.indexOf("@");
+    let dots=mail.lastIndexOf(".");
+    if (mail=='') {
+        $("#valMail").html("* Email masih kosong");
+        $("#valMail").css("display","block")
+        $("#valMail").css("color","red")
+    }
+    else if (atps<1 || dots<atps+2 ) {
+        $("#valMail").html("* Email tidak valid");
+        $("#valMail").css("display","block")
+        $("#valMail").css("color","red")
+        return false;
+    }
+    else {
+        $("#valMail").html("* Email valid");
+        $("#valMail").css("display","block")
+        $("#valMail").css("color","green")
+        return true;
+    }
 });
