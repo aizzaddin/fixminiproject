f51f314cd549308ecc25d67e3b2c9ea4d895d7b6
Wildan Aizzaddin <aizzaddin@gmail.com>
2021-03-12 18:34:47 +0700

    change res to render landingpage.ejs

--- routes/landingPageRoute.js
+++ routes/landingPageRoute.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express.Router()
 
 app.get('/', (req, res) => { 
-    res.send('ini landing page')
+    res.render('landingpage')
 })
 
 module.exports = app
