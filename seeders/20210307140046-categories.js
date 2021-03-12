6f1ddfdf9d9d18a0aab5485415f986acac00fa3e
dwiky <dwiky.darmawansyah@gmail.com>
2021-03-12 22:02:11 +0700

    nambahin seeders baru

--- seeders/20210307140046-categories.js
+++ seeders/20210307140046-categories.js
@@ -3,20 +3,20 @@ const { nanoid } = require('nanoid')
 module.exports = {
     up: async(queryInterface, Sequelize) => {
         await queryInterface.bulkInsert('Categories', [                                                                                               
-            {id: nanoid(), category: 'Ekonomi dan Bisnis'},                                                                                                                                                                                                                                                                                            
-            {id: nanoid(), category: 'Teknik'},                                                                                                                                                                                                                                                                                                        
-            {id: nanoid(), category: 'Matematika dan IPA'},                
-            {id: nanoid(), category: 'Filsafat'},                         
-            {id: nanoid(), category: 'Hukum'},                      
-            {id: nanoid(), category: 'Ilmu Sosial dan Politik'},                                               
-            {id: nanoid(), category: 'Kedokteran dan Ilmu Kesehatan'},                
-            {id: nanoid(), category: 'Kegunaan dan Ilmu Pendidikan'},               
-            {id: nanoid(), category: 'Kehutanan'},                                                                                                                                                       
-            {id: nanoid(), category: 'Pertanian'},                
-            {id: nanoid(), category: 'Peternakan'},                
-            {id: nanoid(), category: 'Psikologi'},                                                                                                                                                                               
-            {id: nanoid(), category: 'Sastra dan Ilmu Budaya'},                                                                                                                                                                                                                                                                                                     
-            {id: nanoid(), category: 'Umum'}               
+            {id: nanoid(), category: 'Ekonomi dan Bisnis', createdAt: new Date(), updatedAt: new Date() },                                                                                                                                                                                                                                                                                            
+            {id: nanoid(), category: 'Teknik', createdAt: new Date(), updatedAt: new Date() },                                                                                                                                                                                                                                                                                                        
+            {id: nanoid(), category: 'Matematika dan IPA', createdAt: new Date(), updatedAt: new Date() },                
+            {id: nanoid(), category: 'Filsafat', createdAt: new Date(), updatedAt: new Date() },                         
+            {id: nanoid(), category: 'Hukum', createdAt: new Date(), updatedAt: new Date() },                      
+            {id: nanoid(), category: 'Ilmu Sosial dan Politik', createdAt: new Date(), updatedAt: new Date() },                                               
+            {id: nanoid(), category: 'Kedokteran dan Ilmu Kesehatan', createdAt: new Date(), updatedAt: new Date() },                
+            {id: nanoid(), category: 'Kegunaan dan Ilmu Pendidikan', createdAt: new Date(), updatedAt: new Date() },               
+            {id: nanoid(), category: 'Kehutanan', createdAt: new Date(), updatedAt: new Date() },                                                                                                                                                       
+            {id: nanoid(), category: 'Pertanian', createdAt: new Date(), updatedAt: new Date() },                
+            {id: nanoid(), category: 'Peternakan', createdAt: new Date(), updatedAt: new Date() },                
+            {id: nanoid(), category: 'Psikologi', createdAt: new Date(), updatedAt: new Date() },                                                                                                                                                                               
+            {id: nanoid(), category: 'Sastra dan Ilmu Budaya', createdAt: new Date(), updatedAt: new Date() },                                                                                                                                                                                                                                                                                                     
+            {id: nanoid(), category: 'Umum', createdAt: new Date(), updatedAt: new Date() }               
         ], {});
     },
     down: async(queryInterface, Sequelize) => {
