be9bc07f7b35b0dd558adcd8067ea1cee0911139
Wildan Aizzaddin <aizzaddin@gmail.com>
2021-03-14 07:16:58 +0700

    fix index and chane static directory

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const path = require('path')
+const fs = require('fs')
 
 app.use(express.json())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname +'/'));
 app.use(cookieParser())
 app.set('view engine', 'ejs');
 
@@ -20,6 +22,7 @@ const serviceRoute = require('./routes/serviceRoute')
 const transactionRoute = require('./routes/transactionRoute')
 const questionRoute = require('./routes/questionRoute')
 const cariRoute = require('./routes/cariRoute')
+const uploadRoute = require('./routes/uploadRoute')
 app.use(landingPageRoute, authRoute)
 
 app.use(function(error, req, res, next) {
@@ -31,8 +34,14 @@ app.use(answerRoute)
 app.use(subscriptionRoute)
 app.use(serviceRoute)
 app.use(transactionRoute)
-app.use(questionRoute)
 app.use(cariRoute)
+app.use('/post', questionRoute)
+app.use(uploadRoute)
+
+const filesDir = path.join(path.dirname(require.main.filename), "uploads");
+if (!fs.existsSync(filesDir)){
+  fs.mkdirSync(filesDir);
+}
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
