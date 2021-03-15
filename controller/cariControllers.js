5c8d6c8c41a43c95d2a19c7f7db70d0fd342fcc9
vamosina <yoppier87@gmail.com>
2021-03-16 01:29:55 +0700

    new ejs restict benerin router

--- controller/cariControllers.js
+++ controller/cariControllers.js
@@ -1,122 +1,91 @@
-const { Courses, Departements, Questions, Answers, Categories } = require('../models')
+const { Users, Courses, Departements, Questions, Answers, Categories } = require('../models')
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
+const moment = require('moment')
+
 
 module.exports = {
-    //nampilin semua judul pertanyaan
-    index: (req, res) => {
-        Questions.findAll({
-                attributes: ['title', 'user_id'],
-                include: [{
-                    attributes: ['id', 'category'],
-                    model: Categories,
-                }],
-            })
-            .then(result => {
-                if (!result[0]) {
-                    res.status(400).json({
-                        success: false,
-                        message: 'Data not found',
-                        data: result
-                    })
-                } else {
-                    res.status(200).json({
-                        success: true,
-                        message: 'Success',
-                        data: result
-                    })
-                }
-
-            })
-    },
-    //searching by title(judul)
-    show: (req, res) => {
-        const { title, question, category_id } = req.query
-        Questions.findAll({
-                limit: 10,
-                where: {
-                    title: {
-                        [Op.iLike]: '%' + title + '%'
-                    },
+    //searching dengan judul tanpa login
+    index: async(req, res) => {
+        const { title } = req.query
+        const questions = await Questions.findAll({
+            where: {
+                title: {
+                    [Op.iLike]: '%' + title + '%'
                 },
-                attributes: ['title', 'user_id', 'question', 'category_id', 'course_id'],
-                include: [{
-                    attributes: ['category'],
-                    model: Categories,
-                }],
-                // attributes: ['user_id', 'answer'],
-                // include: [{
-                //     attributes: ['user_id', 'answer'],
-                //     model: Answers,
-                // }]
-            })
-            .then(result => {
-                if (!result) {
-                    res.status(400).json({
-                        success: false,
-                        message: 'Data not found',
-                        data: result
-                    })
-                } else {
-                    res.render('search', {result: result})
-                }
-
-            })
+            },
+            order: [
+                ['createdAt', 'DESC'], // Sorts by COLUMN_NAME_EXAMPLE in ascending order
+            ],
+            include: [{
+                model: Users
+            }]
+        })
+        const categories = await Categories.findAll({
+            attribute: ['category']
+        })
+        const departements = await Departements.findAll({
+            attribute: ['departement']
+        })
+        const courses = await Courses.findAll({
+            attribute: ['course']
+        })
+        res.render('searchwlogin', { questions, moment, categories, departements, courses })
+    },
+    //searching dengan judul
+    show: async(req, res) => {
+        const { title } = req.query
+        const questions = await Questions.findAll({
+            where: {
+                title: {
+                    [Op.iLike]: '%' + title + '%'
+                },
+            },
+            order: [
+                ['createdAt', 'DESC'], // Sorts by COLUMN_NAME_EXAMPLE in ascending order
+            ],
+            include: [{
+                model: Users
+            }]
+        })
+        const categories = await Categories.findAll({
+            attribute: ['category']
+        })
+        const departements = await Departements.findAll({
+            attribute: ['departement']
+        })
+        const courses = await Courses.findAll({
+            attribute: ['course']
+        })
+        const nama = await Users.findOne({
+            where: { id: req.user.id }
+        })
+        res.render('search', { questions, moment, categories, departements, courses, nama })
     },
     //filtering by categories_matakuliah
-    filter: (req, res) => {
-        const { category, departements, courses, title } = req.body
-        Categories.findAll({
-                attributes: ['category', 'id'],
+    filter: async(req, res) => {
+        const { departementss } = req.query
 
-                where: {
-                    category: {
-                        [Op.iLike]: `%${category}%`
-                    }
+        const categories = await Categories.findAll({
+
+        })
+        const departements = await Departements.findAll({
+            where: {
+                departement: {
+                    [Op.iLike]: '%' + departementss + '%'
                 },
-                include: [{
-                    where: {
-                        departement: {
-                            [Op.iLike]: `%${departements}%`,
-                        }
-                    },
-                    attributes: ['departement', 'category_id', 'id'],
-                    model: Departements,
-                    include: [{
-                        where: {
-                            course: {
-                                [Op.iLike]: `%${courses}%`,
-                            }
-                        },
-                        attributes: ['course', 'id'],
-                        model: Courses,
-                        include: [{
-                            model: Questions,
-                            where: {
-                                title: {
-                                    [Op.iLike]: `%${title}`
-                                }
-                            },
-                            // attributes: ['title'],
-                        }],
-                    }],
-                }],
-            })
-            .then(result => {
-                if (!result[0]) {
-                    res.status(400).json({
-                        success: false,
-                        message: 'Data not found',
-                        data: result
-                    })
-                } else {
-                    res.status(200).json({
-                        success: true,
-                        message: 'Success',
-                        data: result
-                    })
-                }
+            },
+            attribute: ['departement']
+        })
+        const courses = await Courses.findAll({
+            attribute: ['course']
+        })
+        const questions = await Questions.findAll({
+            include: [{
+                model: Users
+            }]
+        })
+        res.render('search', { questions, moment, categories, departements, courses })
+    },
 
-            })
-    }
 }
--- routes/cariRoute.js
+++ routes/cariRoute.js
@@ -1,11 +1,12 @@
 const router = require("express").Router()
-
-// load controller
+const passport = require('../middleware/passportMiddleware')
+const restrict = passport.authenticate('jwt', { session: false, failureRedirect: '/login' })
+    // load controller
 const cariController = require("../controller/cariControllers");
 
 // set endpoint url
 router.get("/search", cariController.index)
-router.get("/cari?", cariController.show)
+router.get("/cari?", restrict, cariController.show)
 router.get("/filter?", cariController.filter)
 
 
